2a3129a440f014e2f8e7cff5f60dc93d7dbad825
S K <84536129+shreyk04@users.noreply.github.com>
2021-06-14 13:14:24 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 let screen=document.getElementById("screen");
-let buttons=document.querySelectorAll("button");
 let screenValue="";
-// for(item of buttons){
-//     item.addEventListener('click',(b)=>{
-//         buttonText=b.target.innerText;
-//         console.log(buttonText);
-        
 
-        // if(buttonText=="X"){
-        //     buttonText="*";
-        //     screenValue+=buttonText;
-        //     screen.value=screenValue;
-        //     console.log(screenValue);
-        // }
         function clear(){
-            
-            // screen.value=" ";
             screen.value="";
             console.log(screen.value);
         }
         function evalu(){
             screen.value=eval(screen.value);
-                        // document.getElementById("screen").value=eval(document.getElementById("screen").value);
-
         }
         function backspace(){
             screenValue=screen.value.slice(0,-1);
@@ -45,26 +29,7 @@ let screenValue="";
             screen.value+=value;
             
         }
-        // function sin(){
-        //     screen.value=Math.sin(screen.value);
-        // }
-        // function cos(){
-        //     screen.value=Math.cos(screen.value);
-        // }
-        // function tan(){
-        //     screen.value=Math.tan(screen.value);
-        // }
-
+        
         
 
 
-//     })
-// }
-// let screen=document.getElementById("screen");
-// function Number(value){
-//     screen.value+=value;
-    
-// }
-// function eval(){
-//  screen.value
-// }
